78508df1f4871071eb5162635676fc2fc54cd247
Jini Park <jinipark@naver.com>
2024-02-03 22:12:01 +0900

    2024-02-03 1

---
+++ user-service0/src/test/java/com/example/userservice/UserService0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserService0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
