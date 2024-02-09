00ae12d782eb7018ebde0b0b78b0f2974885d3fd
Jini Park <jinipark@naver.com>
2024-02-09 15:15:58 +0900

    2024-02-09 1

---
+++ my-second-service/src/test/java/com/example/secondservice/MySecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
