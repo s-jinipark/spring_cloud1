89ee2380ae4a4c2c9407bcbfedfe7e7b8e785e95
Jini Park <jinipark@naver.com>
2024-02-09 18:55:41 +0900

    0209 7

--- my-first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ my-first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,10 +1,13 @@
 package com.example.firstservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -12,6 +15,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FirstServiceController {
 
+	Environment env;
+	
+	@Autowired
+	public FirstServiceController(Environment env) {
+		this.env = env;
+	}
+	
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the First service.";
@@ -24,7 +34,12 @@ public class FirstServiceController {
 	}
 
 	@GetMapping("/check")
-	public String check() {
-		return "Hi, there. This is a message from First Service.";
+	//public String check() {
+	public String check(HttpServletRequest request) {  // mvc 에서는 선언만 해주면 사용 가능
+		//return "Hi, there. This is a message from First Service.";
+		
+		log.info("Server port={}", request.getServerPort());
+		return String.format("Hi, there. This is a message from First Service on PORT %s."
+				, env.getProperty("local.server.port"));  // 2가지 방법
 	}
 }
