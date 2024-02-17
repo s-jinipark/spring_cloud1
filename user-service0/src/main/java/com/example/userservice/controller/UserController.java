1664778e59733026c9644a32304d664e1d9f4796
Jini Park <jinipark@naver.com>
2024-02-17 14:27:18 +0900

    0217 1

--- user-service0/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service0/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,25 +1,46 @@
 package com.example.userservice.controller;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
 	//[1]
+//	private Environment env;
+	
+//	@Autowired
+//	public UserController(Environment env) {
+//		this.env = env;
+//	}
+	//--> [1]
+	
+	//[1] + [3]
 	private Environment env;
+	private UserService userService;
 	
 	@Autowired
-	public UserController(Environment env) {
+	public UserController(Environment env, UserService userService) {
 		this.env = env;
+		this.userService = userService;
 	}
-	//--> [1]
+	//--> [1] + [3]
 	
 	//[2] 두번째 방법
 	@Autowired
@@ -35,6 +56,42 @@ public class UserController {
 	public String welcome() {
 		//return env.getProperty("greeting.message"); // -> [1]
 		return greeting.getMessage(); // -> [2]
+	}
+	
+	// [3] => jpa
+//	@PostMapping("/users")
+//	public String createUser(@RequestBody RequestUser user) {
+//		ModelMapper mapper = new ModelMapper();
+//		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+//		
+//		UserDto userDto = mapper.map(user, UserDto.class);
+//		userService.createUser(userDto);
+//		
+//		return "Create user method is called";
+//	}
+	// [3-2] => jpa + return 변경
+//	@PostMapping("/users")
+//	public ResponseEntity createUser(@RequestBody RequestUser user) {
+//		ModelMapper mapper = new ModelMapper();
+//		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+//		
+//		UserDto userDto = mapper.map(user, UserDto.class);
+//		userService.createUser(userDto);
+//		 
+//		return new ResponseEntity(HttpStatus.CREATED);
+//	}
+	// [3-3] 
+	@PostMapping("/users")
+	public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserDto userDto = mapper.map(user, UserDto.class);
+		userService.createUser(userDto);
+		
+		ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
 	}
 	
 }
---
+++ user-service0/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.userservice.service;
+
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
+import com.example.userservice.jpa.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		// TODO Auto-generated method stub
+		userDto.setUserId(UUID.randomUUID().toString());
+		
+		// 하나의 객체를 -> 다른 객체로 (일일이 get, set 하기 어려우므로..)
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		// 전략 -> 딱 맞아 떨어지게..
+		UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+		userEntity.setEncryptedPwd("encrypted_password");
+		
+		userRepository.save(userEntity);
+
+		// [3-3]
+		//return null;
+		UserDto returnUserDto = mapper.map(userEntity, UserDto.class);
+		return returnUserDto;
+	}
+
+}
